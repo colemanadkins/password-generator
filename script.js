86205c13b8a34538e8796d4cc1c6e327758b7c65
Coleman Adkins <ladiesluvcoleman@yahoo.com>
2021-12-23 09:40:44 -0500

    Deleted redundant comments and variables

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assignment Code
 // Line below referencing the class #generate
 var generateBtn = document.querySelector("#generate");
 // Password criteria variables
@@ -6,12 +5,7 @@ var specialCharacters = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?",
 var numericCharacters = [0,1,2,3,4,5,6,7,8,9];
 var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 var upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-var hasSpecialCharacters;
-var hasNumbers;
-var hasLowerCase;
-var hasUpperCase;
 var choices;
-var randomPassword = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -24,20 +18,16 @@ function writePassword() {
 function generatePassword() {
   var passwordLength = prompt("Please enter password length between 8-128 characters")
   console.log(passwordLength);
-// Add if statement for not meeting password requirements
+  // Add if statement for not meeting password requirements
   if (passwordLength <8 || passwordLength >128) {
    prompt("Please enter 8-128 characters");
-
+  // Else move forward 
   } else {
   var hasNumbers = confirm("Include Numbers?");
-  console.log(hasNumbers);
   var hasSpecialCharacters = confirm("Include special character?");
-  console.log(hasSpecialCharacters);
   var hasUpperCase = confirm("Include uppercase?");
-  console.log(hasUpperCase);
   var hasLowerCase = confirm("Include lowercase?");
-  console.log(hasLowerCase);
-  };
+  }
   // If statement if no criteria has been chosen
   if (!hasNumbers && !hasSpecialCharacters && !hasUpperCase && !hasLowerCase) {
     choices = alert("You mush choose a criteria!");
@@ -91,15 +81,20 @@ function generatePassword() {
   else if (hasLowerCase) {
     choices = lowerCasedCharacters;
   }
+  // Initialize random password array
+  var randomPassword = [];
+  // Looping for the number of "characters" selected for the password length
   for (var i = 0; i < passwordLength; i++) {
-    var pickChoices = choices[Math.floor(Math.random() *choices.length)];
-    randomPassword.push(pickChoices);
+    var pickChoices = choices[Math.floor(Math.random() *choices.length)];//Choosing the random character from choices array
+    randomPassword.push(pickChoices);//Adding selected character to the random password array
   }
-  var ps = randomPassword.join("");
-  return ps
+  var ps = randomPassword.join("");//Joining all character in the array to make one string
+  return ps // Returns joined string to the writePassword function
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// See Note1
+
+//Does there need to be a prompt for misentering characters?
+//
 
